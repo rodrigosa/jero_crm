1bb9c10dd7beace8fa060ccdd1b131c7b5486872
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-01-20 23:11:53 -0300

    Profiles de application.properties e anotations para profiles

--- src/main/java/io/github/rodrigosa/configurations/InitialConfiguration.java
+++ src/main/java/io/github/rodrigosa/configurations/InitialConfiguration.java
@@ -1,14 +1,16 @@
 package io.github.rodrigosa.configurations;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
+@Development
 public class InitialConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Jero_CRM";
-
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO!");
+        };
     }
 }
