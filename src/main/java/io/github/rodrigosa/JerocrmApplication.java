b1459dfbec0805614c400063597b1b62664ade4e
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-01-20 22:39:53 -0300

    Segundo commit (application.properties)

--- src/main/java/io/github/rodrigosa/JerocrmApplication.java
+++ src/main/java/io/github/rodrigosa/JerocrmApplication.java
@@ -1,7 +1,6 @@
 package io.github.rodrigosa;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class JerocrmApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+   @Value("${application.name}")
     private String applicationName;
 
     public static void main(String[] args) {
