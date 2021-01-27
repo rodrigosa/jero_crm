511e2134ad5cd7e6fb0a371e238b866cb1d51494
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-01-27 18:13:25 -0300

    REST - GET Cliente

--- src/main/java/io/github/rodrigosa/JerocrmApplication.java
+++ src/main/java/io/github/rodrigosa/JerocrmApplication.java
@@ -1,10 +1,25 @@
 package io.github.rodrigosa;
 
+import io.github.rodrigosa.domain.entity.Cliente;
+import io.github.rodrigosa.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JerocrmApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+        return args ->{
+            Cliente c = new Cliente(null, "Fulano");
+            clientes.save(c);
+
+
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JerocrmApplication.class, args);
--- src/main/java/io/github/rodrigosa/rest/controller/ClienteController.java
+++ src/main/java/io/github/rodrigosa/rest/controller/ClienteController.java
@@ -1,18 +1,32 @@
 package io.github.rodrigosa.rest.controller;
 
+import io.github.rodrigosa.domain.entity.Cliente;
+import io.github.rodrigosa.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/api/clientes")
+//@RequestMapping("/api/clientes")
 public class ClienteController {
 
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
     @ResponseBody
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
-    public String helloCliente(@PathVariable("nome") String nomeCliente){
-        return String.format("Hello %s", nomeCliente);
+    @GetMapping("/api/clientes/{id}")
+    public ResponseEntity getClienteById(@PathVariable Integer id) {
+
+        Optional<Cliente> cliente = clientes.findById(id);
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
