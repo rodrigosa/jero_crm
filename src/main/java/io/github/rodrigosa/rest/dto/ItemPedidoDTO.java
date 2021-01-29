8af39b295bc459bc4628e7d35a68ed1f3bcb6c16
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-01-29 12:09:34 -0300

    REST - Gravando um pedido RegraNegocioException PedidoServiceImpl
    (CORRIGIDO) - rEMOVIDO A ANOTAÇÃO @Entity de PedidoDTO e ItemPedidoDTO

--- src/main/java/io/github/rodrigosa/rest/dto/ItemPedidoDTO.java
+++ src/main/java/io/github/rodrigosa/rest/dto/ItemPedidoDTO.java
@@ -5,10 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
 
 @Data
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class ItemPedidoDTO {
--- src/main/java/io/github/rodrigosa/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/rodrigosa/rest/dto/PedidoDTO.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
-@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 public class PedidoDTO {
