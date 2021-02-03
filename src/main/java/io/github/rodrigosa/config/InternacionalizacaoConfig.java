875ab874c97f5a241c434c746968c2921201c8ed
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-02-03 12:39:11 -0300

    Internacionalizacao (Message.properties)

---
+++ src/main/java/io/github/rodrigosa/config/InternacionalizacaoConfig.java
@@ -0,0 +1,23 @@
+package io.github.rodrigosa.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+import java.util.Locale;
+
+@Configuration
+public class InternacionalizacaoConfig {
+
+    @Bean
+    public MessageSource messageSource(){
+
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("ISO-8859-1");
+        messageSource.setDefaultLocale(Locale.getDefault());
+        return messageSource;
+
+    }
+}
