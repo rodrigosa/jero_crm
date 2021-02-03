084e3f2f6ca807c8f3ea273843c11272d2f55f53
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-02-03 14:43:55 -0300

    Implementação do JWT

--- src/main/java/io/github/rodrigosa/config/SecurityConfig.java
+++ src/main/java/io/github/rodrigosa/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package io.github.rodrigosa.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -11,19 +14,44 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * Traz os objetos que vão fazer a autenticação dos usuários e adicionar estes usuários dentro do contexto
      * do security
-     * @param auth
+     *
      * @throws Exception
      */
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); //Algoritimo de autenticação
+    }
 
     //Autenticação
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("fulano")
+                .password(passwordEncoder().encode("123"))
+                .roles("USER");
     }
 
     //Autorização
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()//desabilitando segurança entre frontend e backend STATELESS
+                .authorizeRequests()
+                .antMatchers("/api/clientes/**")
+                .authenticated()
+                .and() //Volta para o primeiro metodo
+                .formLogin(); //Aula 69 - 8:50
+        ;
     }
+    /**
+     * <form method="post">
+     *     <input type="text" name="username"
+     *     <input type="secret" name="password"
+     *      <button type="submit"...
+     * </form>
+     *
+     * .formLogin("/meu-login.html"); //Aula 69 - 8:50
+     */
 }
