66cb03eb54095a76930a737f5aab1bae543b4d53
rodrigodeoliveirasa@gmail.com <rodrigodeoliveirasa@gmail.com>
2021-02-10 11:02:24 -0300

    Implementação do Swagger

--- src/main/java/io/github/rodrigosa/rest/controller/ClienteController.java
+++ src/main/java/io/github/rodrigosa/rest/controller/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.rodrigosa.rest.controller;
 
 import io.github.rodrigosa.domain.entity.Cliente;
 import io.github.rodrigosa.domain.repository.Clientes;
+import io.swagger.annotations.*;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Api("Api Clientes")
 public class ClienteController {
 
     private Clientes clientes;
@@ -23,7 +25,12 @@ public class ClienteController {
     }
 
     @GetMapping("{id}")
-    public Cliente getClienteById(@PathVariable Integer id) {
+    @ApiOperation("Obter detalhes de um cliente.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente encontrado"),
+            @ApiResponse(code = 404, message = "Cliente não encontrado para o ID informado")
+    })
+    public Cliente getClienteById(@PathVariable @ApiParam("ID do cliente") Integer id) {
 
         return clientes
                 .findById(id)
@@ -33,6 +40,11 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Salva novo cliente.")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cliente salvo com sucesso"),
+            @ApiResponse(code = 400, message = "Erro de validação")
+    })
     public Cliente save(@RequestBody @Valid Cliente cliente) { // @RequestBody -> diz que o parametro vem no corpo da requsicao
 
         return clientes.save(cliente);
